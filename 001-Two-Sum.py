8111655e06100a4fb514c7ef9ced86e6de2a4d5e
Rex <m41045@gmail.com>
2019-02-26 20:45:54 +0800

    topic

--- 001-Two-Sum.py
+++ 001-Two-Sum.py
@@ -1,8 +1,2 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 26 20:42:25 2019
-
-@author: rex
-"""
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
