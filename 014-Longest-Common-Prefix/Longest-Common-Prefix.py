13e273080c5e18ce0381dc945be8b2433d218a59
Rex <m41045@gmail.com>
2019-03-02 23:24:56 +0800

    Modify the solution

--- 014-Longest-Common-Prefix/Longest-Common-Prefix.py
+++ 014-Longest-Common-Prefix/Longest-Common-Prefix.py
@@ -1,54 +1,29 @@
 class Solution:
     def longestCommonPrefix(self, strs):      
-        n = 0
-        
-        if len(strs) == 0 or strs[0] == "":
+        # 若為空集合或有空元素返回" "
+        if (len(strs) == 0) or ("" in strs):
             return ""
-        
+        # 若只有單一元素返回本身
         elif len(strs) == 1:
-            return strs[0]
-        
-        elif "" in strs:
-            return ""
-        
-        elif len(strs) == 2:
-            ps = strs[0]
-            low = min(len(strs[0]),len(strs[1]))
-            while (n <= low) and (min(len(strs[0]),len(strs[1]))!=0) :
-                for i in range(0, min(len(strs[0]),len(strs[1]))):
-                    if (i != 0) and (n == 0):
-                        break
-                    if (strs[0].find(strs[1][i]) >= 0) and (strs[0].find(strs[1][i]) == strs[1].index(strs[1][i])):
-                        #print(i)
-                        n +=1
-                        strs[0] = strs[0].replace(strs[0][0],"",1)
-                        strs[1] = strs[1].replace(strs[1][0],"",1)
-                        break
-                if (n == 0) or ("" in strs) or (strs[0][0] != strs[1][0]):
-                    break
-            if n==0:
-                ps=""
-            return ps[:n]
-        
-        elif len(strs)>2:            
-            ps = strs[0]        
-            while (n % (len(strs)-1) ==0) and (min(len(strs[0]),len(strs[1]))!=0):
+            return strs[0]       
+        # 兩兩比對第一個字母，比對完後若為相同，刪除第一個字母
+        else:            
+            ps = ""
+            while True :                
                 for i in range(0, len(strs)-1):
-                    if (strs[i].find(strs[i+1][0]) >= 0) and (strs[i].find(strs[i+1][0]) == strs[i+1].index(strs[i+1][0])):
-                        n +=1
+            
+                    if strs[i][0] == strs[i+1][0]:
                         strs[i] = strs[i].replace(strs[i][0],"",1)
                         if (i+2) == len(strs):
+                            ps += strs[i+1][0]
                             strs[i+1] = strs[i+1].replace(strs[i+1][0],"",1)
-                if (n == 0) or ("" in strs) or (strs[0][0] != strs[1][0]):
-                    break
 
-            if (n % 2 != 0) and (len(strs)%3==0):
-                n-=1
-            if n==0:
-                ps=""
-            return ps[:n//(len(strs)-1)]
-    
+                    else:                        
+                        return ps
+                    
+                if "" in strs:
+                            return ps
     
 if __name__ == "__main__":
     s = Solution
-    print(s.longestCommonPrefix(s,["flower","flow","flight"]))
+    print(s.longestCommonPrefix(s,["a","a","a"]))
