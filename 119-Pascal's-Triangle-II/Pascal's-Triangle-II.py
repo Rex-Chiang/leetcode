106e2502545623a51d4f92827c29809f35e3a2e4
Rex-Chiang <39951287+Rex-Chiang@users.noreply.github.com>
2020-02-03 17:31:35 +0800

    Solution of 119-Pascal's-Triangle-II

---
+++ 119-Pascal's-Triangle-II/Pascal's-Triangle-II.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getRow(self, rowIndex):
+        if rowIndex == 0:
+            return [1]
+
+        tmp = [1]
+        res = []
+        
+        while len(res) < rowIndex + 1:
+            res = []
+            tmp = [0] + tmp + [0]
+            
+            for i in range(0, len(tmp)-1):
+                res.append(tmp[i]+tmp[i+1])
+            
+            tmp = res
+            
+        return res
+        
+if __name__ == "__main__":
+    s = Solution()
+    print(s.getRow(rowIndex = 5))
